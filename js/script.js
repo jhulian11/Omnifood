85125a29daa85623bd9d3d9971da1e805c763864
Jhulian11 <jhuliangamer@gmail.com>
2024-01-14 18:20:07 -0300

    Making Mobile Navigation Button work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const yearEl = document.querySelector('.year');
 
 yearEl.textContent = new Date().getFullYear();
+
+const headerEl = document.querySelector('.header');
+
+const btnMobileNav =  headerEl.querySelector('.btn-mobile-nav');
+
+btnMobileNav.addEventListener('click', () => {
+    headerEl.classList.toggle('nav-open');
+});
