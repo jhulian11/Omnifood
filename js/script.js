d797076ad9d532be0147cb8105b956dd6d88e653
Jhulian11 <jhuliangamer@gmail.com>
2024-01-15 22:41:00 -0300

    Implementing smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,11 +1,62 @@
-const yearEl = document.querySelector('.year');
+const omnifood = () => {
+    const headerEl = document.querySelector('.header');
 
-yearEl.textContent = new Date().getFullYear();
+    const yearUpdate = () =>{
+        const yearEl = document.querySelector('.year');
+    
+        yearEl.textContent = new Date().getFullYear();
+    };
+    
+    const  headerMobileAction = () => {
+    
+        const btnMobileNav =  headerEl.querySelector('.btn-mobile-nav');
+    
+        btnMobileNav.addEventListener('click', () => {
+            headerEl.classList.toggle('nav-open');
+        });
+    };
 
-const headerEl = document.querySelector('.header');
+    const smoothScrolling = () => {
+        const allLinks = document.querySelectorAll('a:link');
 
-const btnMobileNav =  headerEl.querySelector('.btn-mobile-nav');
+        allLinks.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
 
-btnMobileNav.addEventListener('click', () => {
-    headerEl.classList.toggle('nav-open');
-});
+                const href = link.getAttribute('href');
+
+                // Scroll back to top
+                if (href === "#")
+                window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+                });
+
+                // Scroll to other links
+                if (href !== "#" && href.startsWith("#")) {
+                const sectionEl = document.querySelector(href);
+                sectionEl.scrollIntoView({ behavior: "smooth" });
+                }
+
+                // Close mobile naviagtion
+                if (link.classList.contains("main-nav-link"))
+                    headerEl.classList.toggle("nav-open");
+
+            });
+        });
+    };
+    
+    const init = () => {
+        yearUpdate();
+        headerMobileAction();
+        smoothScrolling();
+    };
+
+    init();
+};
+
+omnifood();
+
+
+
+
